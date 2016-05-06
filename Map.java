a569f48a166f1bcb5addf872740ce12d2b1cd3b5
manorell <manorell@kth.se>
2016-05-06 14:45:59 +0200

    Tower defence map

---
+++ Map.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Map {
+
+public static void main(String args[]){
+        JFrame frame = new JFrame("Map");
+        frame.setSize(400,500);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
