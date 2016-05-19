a398a4df9791bfa05a777e1554edff6f7a87aa8e
simonba <simonba@kth.se>
2016-05-19 19:59:32 +0200

    Starting to be able to print the board

--- Game.java
+++ Game.java
@@ -1,15 +1,50 @@
-
+import java.util.List;
+import java.util.ArrayList;
 
 
 public class Game {
 
-    public Game() {
+    private int X_SIZE;
+    private int Y_SIZE;
+    private final Board board;
+    private List<Tower> towers;
+    private List<Enemy> enemies;
 
+    public Game() {
+        towers = new ArrayList<Tower>();
+        enemies = new ArrayList<Enemy>();
+        board = createBoard();
+        enemies.add(new Enemy(board, 0, 1));
     }
 
+
+    private Board createBoard() {
+        Board newBoard = new Board(X_SIZE, Y_SIZE);
+        newBoard.addStartingPoint(0,1);
+        newBoard.addStartingPoint(4,1);
+        newBoard.addEndingPoint(4,3);
+        newBoard.connect(0, 1, 1, 1);
+        newBoard.connect(1, 1, 2, 1);
+        newBoard.connect(2, 1, 3, 1);
+        newBoard.connect(3, 1, 4, 1);
+        newBoard.connect(2, 1, 2, 2);
+        newBoard.connect(2, 2, 2, 3);
+        newBoard.connect(2, 3, 3, 3);
+        newBoard.connect(3, 3, 4, 3);
+        return newBoard;
+    }
+
+    public void dump() {
+        for(int y = 0; y < Y_SIZE; y++) {
+            for(int x = 0; x < X_SIZE; x++) {
+
+            }
+
+        }
+    }
 
     public static void main (String[] args) {
-
-
+        Game game = new Game();
     }
+
 }
