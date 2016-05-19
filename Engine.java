79985efedfa35f6afc414abe313d3be5b584a921
simonba <simonba@kth.se>
2016-05-19 21:13:01 +0200

    Remove the survivors when they get across the board

--- Engine.java
+++ Engine.java
@@ -1,4 +1,6 @@
+import java.util.Iterator;
 import java.util.List;
+
 
 
 
@@ -9,12 +11,23 @@ public class Engine {
     }
 
     public void act(List<Enemy> enemies) {
-
+        moveEnemies(enemies);
+        removeSurvivors(enemies);  
     }
 
     private void moveEnemies(List<Enemy> enemies) {
         for (Enemy enemy : enemies) {
             enemy.move();
+        }
+    }
+
+    private void removeSurvivors(List<Enemy> enemies) {
+        Iterator<Enemy> enemyIterator = enemies.iterator();
+        while (enemyIterator.hasNext()) {
+            Enemy enemy = enemyIterator.next();
+            if (enemy.isSurvivor() || enemy.isAlive()) {
+                enemyIterator.remove();
+            }
         }
     }
 }
