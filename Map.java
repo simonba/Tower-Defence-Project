8095ffb68e04fcf977d91915ad9bd816cd63aca1
simonba <simonba@kth.se>
2016-05-25 12:57:48 +0200

    Comment in the Map class

--- Map.java
+++ Map.java
@@ -1,6 +1,8 @@
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
+//This class just pops up a window. It was planned to be able to play the game here also....
+
 public class Map {
 
     public static void main(String args[]) {
