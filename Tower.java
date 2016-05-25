0314923570a51aa5485c32583943f535def150fe
simonba <simonba@kth.se>
2016-05-25 12:23:44 +0200

    Documentation in Tower

--- Tower.java
+++ Tower.java
@@ -11,10 +11,22 @@ public class Tower {
         this.range = range;
     }
 
+    /**
+     * A method that checks if a given location holds any towers
+     * or enemies. Returns true if something is found, false otherwise.
+     *
+     * @param x the position in x-direction
+     * @param y the position in y-direction
+     * @return true if there is a match and false otherwise.
+     */
     public boolean match(int x, int y) {
         return location.x == x && location.y == y;
     }
 
+    /**
+     * The towers should hit targets if they are in range. This is not
+     * implemented properly
+     */
     public static void hitTargets() {
         if (location.distance(location.x, location.y, Enemy.location.x, Enemy.location.y) < 2.0) {
             Enemy.hit();
@@ -22,3 +34,4 @@ public class Tower {
     }
 }
 
+ 
