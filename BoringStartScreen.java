d14db5a0b9c86f4adf93645424f05c0be57b4de4
simonba <simonba@kth.se>
2016-06-08 17:41:44 +0200

    Astarting screen for the game

---
+++ BoringStartScreen.java
@@ -0,0 +1,27 @@
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class BoringStartScreen extends BasicGameState {
+
+
+    public void init(GameContainer container, StateBasedGame game) throws SlickException {
+
+    }
+
+    public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+
+    }
+
+    public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+
+    }
+
+
+    public int getID() {
+        return 0;
+    }
+}
