c880a776115e158a04d6d78fbe6e81d69cb48bf9
simonba <simonba@kth.se>
2016-06-08 17:03:35 +0200

    Making the GameState basic methods

--- TowerDefence.java
+++ TowerDefence.java
@@ -1,23 +1,24 @@
 import org.newdawn.slick.*;
 import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 
-public class TowerDefence extends StateBasedGame {
+public class TowerDefence extends BasicGameState {
 
     private Shape circle = null;
 //    private ArrayList<Enemy> enemies;
-
 
 
     public TowerDefence(String title) {
         super(title);
     }
 
-    public void initSateList(GameContainer gc) throws SlickException {
+
+    public void initStatesList(GameContainer gc) throws SlickException {
         circle = new Circle(5, 100, 15);
-  
+
 
     }
 
