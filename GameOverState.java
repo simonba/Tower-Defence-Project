17e97fa7c43c676449155feff290a0e2a7bdbc6e
simonba <simonba@kth.se>
2016-06-08 17:22:44 +0200

    Game Over window

---
+++ GameOverState.java
@@ -0,0 +1,31 @@
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class GameOverState extends BasicGameState {
+
+
+    public void init(GameContainer container, StateBasedGame game) throws SlickException {
+
+    }
+
+
+
+    public void update(GameContainer gc, StateBasedGame state, int delta) throws SlickException {
+
+    }
+
+
+    public void render(GameContainer container, StateBasedGame state, Graphics g) throws SlickException {
+
+
+    }
+
+
+    public int getID() {
+        return 0;
+    }
+
+}
